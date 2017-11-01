806e2843005900f33df6f106217ceba1140f8c84
aureliad22 <aurelia.delaune@gmail.com>
2017-11-01 17:08:32 +0100

    test pour jenkins

---
+++ src/test/java/fr/eni_ecole/dao/CarDAOImplTest.java
@@ -0,0 +1,24 @@
+/**
+ * @author oreade
+ * @date 1 nov. 2017
+ * @version TestPOI V1.0
+ */
+package fr.eni_ecole.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author oreade
+ * @date 1 nov. 2017
+ * @version TestPOI V1.0
+ */
+public class CarDAOImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
