862e8b3396bc7e114b814c3b671fa4a60ef33f22
aureliad22 <aurelia.delaune@gmail.com>
2017-11-01 18:53:20 +0100

    tests unit

---
+++ src/test/java/fr/eni_ecole/dao/DriverDAOImplTest.java
@@ -0,0 +1,95 @@
+/**
+ * @author oreade
+ * @date 1 nov. 2017
+ * @version TestPOI V1.0
+ */
+package fr.eni_ecole.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author oreade
+ * @date 1 nov. 2017
+ * @version TestPOI V1.0
+ */
+public class DriverDAOImplTest {
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.DriverDAOImpl#insert(fr.eni_ecole.models.Driver)}.
+	 */
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.DriverDAOImpl#update(fr.eni_ecole.models.Driver)}.
+	 */
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.DriverDAOImpl#delete(fr.eni_ecole.models.Driver)}.
+	 */
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.DriverDAOImpl#selectById(int)}.
+	 */
+	@Test
+	public void testSelectById() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.DriverDAOImpl#selectAll()}.
+	 */
+	@Test
+	public void testSelectAll() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/fr/eni_ecole/dao/LicenseDAOImplTest.java
@@ -0,0 +1,119 @@
+/**
+ * @author oreade
+ * @date 1 nov. 2017
+ * @version TestPOI V1.0
+ */
+package fr.eni_ecole.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author oreade
+ * @date 1 nov. 2017
+ * @version TestPOI V1.0
+ */
+public class LicenseDAOImplTest {
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Méthode en charge de 
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#insert(fr.eni_ecole.models.DrivingLicense)}.
+	 */
+	@Test
+	public void testInsert() {
+		assertEquals(3, 3);
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#update(fr.eni_ecole.models.DrivingLicense)}.
+	 */
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#delete(fr.eni_ecole.models.DrivingLicense)}.
+	 */
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#selectById(int)}.
+	 */
+	@Test
+	public void testSelectById() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#selectAll()}.
+	 */
+	@Test
+	public void testSelectAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#importAll(java.lang.String)}.
+	 */
+	@Test
+	public void testImportAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#importAllIntoDB(java.lang.String)}.
+	 */
+	@Test
+	public void testImportAllIntoDB() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link fr.eni_ecole.dao.LicenseDAOImpl#exportAll(java.lang.String)}.
+	 */
+	@Test
+	public void testExportAll() {
+		fail("Not yet implemented");
+	}
+
+}
